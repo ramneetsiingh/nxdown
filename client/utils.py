3df6431e3e4051915c9f4e5f202eb8eef1afa28c
ramneet <ramneetsiingh@gmail.com>
2020-06-24 15:53:14 +0530

    client

--- client/utils.py
+++ client/utils.py
@@ -1,7 +1,27 @@
+import os
+import pickle
+
 def size_format(b):
+    '''Convert bytes to a representative format. Example: size_format(1024) returns "1 KB" '''
     unit = ['B','KB','MB','GB','TB']
     i = 0
     while b/1024 >= 1:
         b /= 1024
         i = i+1
     return f"{b:.3f} {unit[i]}"
+
+def mkdir(path):
+    '''make directory if does not exist'''
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def write_pkl(data, file_path):
+    '''Serialize data by usign pickle and save at given location'''
+    with open(file_path, 'wb') as f:
+        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+
+def read_pkl(file_path):
+    '''Read pickel file and return'''
+    with open(file_path, "rb") as f:
+        data = pickle.load(f)
+    return data
--- client/workshop.py
+++ client/workshop.py
@@ -1,60 +1,94 @@
-import os
-import pickle
 import requests
-import config as conf
-from utils import size_format
+import config
+import os
+import utils
 
+appdata_dir = config.directory.get('APPDATA')
+utils.mkdir(appdata_dir)
 
-f_id = int(input('Enter Factory ID : '))
-w_id = int(input('Enter your worker\'s ID : '))
+def worker_path(w_id):
+    return os.path.join(appdata_dir,str(w_id))
 
-chunks_path = os.path.join(os.getcwd(),conf.download['base_dir_name'])
-download_path = os.path.join(chunks_path,str(w_id))
+#------------------------------------------------------------------------------
 
-hf =  open(os.path.join(download_path,'head.pkl'), 'rb' )
-head = pickle.load(hf)
-hf.close()
+def init_worker_dir(head):
+    worker_dir = worker_path(head['worker_id'])
+    utils.mkdir(worker_dir)
+    utils.write_pkl(head, os.path.join(worker_dir, "head"))
+    utils.write_pkl({'global_status' : 'pending'}, os.path.join(worker_dir, "work"))
 
-url = head['url']
-factory_id = head['factory_id']
-worker_id = head['worker_id']
+def initFactory(url):
+    initFactory = config.getURL('initFactory')
+    r = requests.post(initFactory, json = {'url' : url}) 
+    response = r.json()
+    init_worker_dir(response)
+    return response
 
+def joinFactory(f_id):
+    joinFactory = config.getURL('joinFactory')
+    r = requests.post(joinFactory, json = {'factory_id' : f_id}) 
+    response = r.json()
+    init_worker_dir(response)
+    return response
+#------------------------------------------------------------------------------
 
+def update_work_file(worker_id, key, value):
+    workfile = os.path.join(worker_path(worker_id), "work")
+    work = utils.read_pkl(workfile)
+    work[key] = value
+    utils.write_pkl(work, workfile)
 
-def download(work_id, start, end):
-    bytes = end-start+1
-    chunk = {'Range': 'bytes={0}-{1}'.format(start, end)}
-    print(f'Downloading Chunk {work_id} ({size_format(bytes)})')
-    r = requests.get(url, headers=chunk, stream=True) 
-    
-    with open(os.path.join(download_path, str(work_id)), 'wb') as fp:
-        fp.write(r.content)
-    print('Done')
-    
-def get_work():    
-    getWork = conf.getURL('getWork')
+def get_work(factory_id, worker_id):    
+    getWork = config.getURL('getWork')
     work = requests.post(getWork, json = {
-        'factory_id' : f_id,
-        'worker_id' : w_id
-    })
-    return work.json()
+        'factory_id' : factory_id,
+        'worker_id' : worker_id
+    }).json()
 
-def submit_work(work_id):
-    submitWork = conf.getURL('submitWork')
+    if work['isWork'] == 1:
+        update_work_file(worker_id, work['work_id'], "pending")
+    else:
+        update_work_file(worker_id, "global_status", "done")
+
+    return work
+
+def submit_work(factory_id, worker_id, work_id):
+    update_work_file(worker_id, work_id, "done")
+    submitWork = config.getURL('submitWork')
     work = requests.post(submitWork, json = {
-        'factory_id' : f_id,
+        'factory_id' : factory_id,
         'work_id' : work_id
     })
 
-work = get_work()
-while work['isWork'] == 1:
-    work_id = work['work_id']
-    a = work['range_start']
-    b = work['range_end']
-    download(work_id, a, b)
-    submit_work(work_id)
-    work = get_work()
+
+def download(url, worker_id, work_id, start, end):
+    bytes = end-start+1
+    chunk = {'Range': 'bytes={0}-{1}'.format(start, end)}
+    print(f'Downloading Chunk {work_id} ({utils.size_format(bytes)})')
+    r = requests.get(url, headers=chunk, stream=True) 
+    
+    with open(os.path.join(worker_path(worker_id), str(work_id)), 'wb') as fp:
+        fp.write(r.content)
+    print('Done')
+
+def resume_download(worker_id):
+    head = utils.read_pkl(os.path.join(worker_path(worker_id), "head"))
+    worker_id = head['worker_id']
+    factory_id = head['factory_id']
+    url = head['url']
+
+    work = get_work(factory_id, worker_id)
+    while work['isWork'] == 1:
+        work_id = work['work_id']
+        a = work['range_start']
+        b = work['range_end']
+        print(f'{a} {b}')
+        download(url, worker_id, work_id, a, b)
+        submit_work(factory_id, worker_id, work_id)
+        work = get_work(factory_id, worker_id)
 
 
-
-# 19 29
+x = initFactory('https://file-examples.com/wp-content/uploads/2017/04/file_example_MP4_480_1_5MG.mp4')
+# print(x['factory_id'])
+# x = joinFactory(18)
+resume_download(x['worker_id'])
